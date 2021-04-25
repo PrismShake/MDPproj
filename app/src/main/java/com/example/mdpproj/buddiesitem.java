026bd5cac846d00d2cc7f1c76c1994345b31c595
Hanna <hannabowow@gmail.com>
2021-04-25 16:39:35 -0400

    workoutandbuddies2

--- app/src/main/java/com/example/mdpproj/buddiesitem.java
+++ app/src/main/java/com/example/mdpproj/buddiesitem.java
@@ -1,6 +1,8 @@
 package com.example.mdpproj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,5 +24,10 @@ public class buddiesitem extends AppCompatActivity{
         personName.setText("Emma Wilson");
         personAge.setText("23 years old");
         personPhoto.setImageResource(R.drawable.mdp_prof_backg);
+    }
+
+    public void goBudProf(View view) {
+        Intent intent = new Intent(view.getContext(), buddiesProf.class);
+        startActivity(intent);
     }
 }
