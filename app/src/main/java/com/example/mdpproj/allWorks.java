5834f63f52aa1e21e3965b192c0a59c6dbdd6ad1
Hanna <hannabowow@gmail.com>
2021-04-30 12:40:19 -0400

    up

--- app/src/main/java/com/example/mdpproj/allWorks.java
+++ app/src/main/java/com/example/mdpproj/allWorks.java
@@ -1,6 +1,8 @@
 package com.example.mdpproj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,4 +13,42 @@ public class allWorks extends AppCompatActivity {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_all_works );
     }
+
+    public void resistanceMach(View view) {
+        Intent a = new Intent (this, profilePage.class );
+        a.putExtra("resWork", R.id.res);
+        startActivity(a);
+    }
+
+    public void benchPress(View view) {
+        Intent b = new Intent (this, profilePage.class );
+        b.putExtra("bench", R.id.bench);
+        startActivity(b);
+    }
+
+    public void yoga(View view) {
+        Intent c = new Intent (this, profilePage.class );
+        c.putExtra("yoga", R.id.yog);
+        startActivity(c);
+    }
+
+    public void freeWeights(View view) {
+        Intent d = new Intent (this, profilePage.class );
+        d.putExtra("free", R.id.bell);
+        startActivity(d);
+    }
+
+    public void treadmill(View view) {
+        Intent e = new Intent (this, profilePage.class );
+        e.putExtra("mill", R.id.tread);
+        startActivity(e);
+    }
+
+    public void cycling(View view) {
+        Intent f = new Intent (this, profilePage.class );
+        f.putExtra("cyc", R.id.bike);
+        startActivity(f);
+    }
+
+
 }
