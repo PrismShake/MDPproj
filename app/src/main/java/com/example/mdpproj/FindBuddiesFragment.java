4ef263b394df65c28d1ff3570358c476c645a6b5
sislam5489 <32273143+sislam5489@users.noreply.github.com>
2021-05-11 15:33:20 -0400

    Tried Fixing Buddies Page

--- app/src/main/java/com/example/mdpproj/FindBuddiesFragment.java
+++ app/src/main/java/com/example/mdpproj/FindBuddiesFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +17,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -23,12 +26,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FindBuddiesFragment extends Fragment implements BuddyAdapter.OnClick {
     RecyclerView mRecyclerView;
     RecyclerView.Adapter mAdapter;
+    private List<BuddiesObject> results = new ArrayList<>();
    // private RecyclerView.LayoutManager mLayoutManager;
     EditText mInput;
 
@@ -42,7 +48,6 @@ public class FindBuddiesFragment extends Fragment implements BuddyAdapter.OnClic
     }
 
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -54,10 +59,10 @@ public class FindBuddiesFragment extends Fragment implements BuddyAdapter.OnClic
         mRecyclerView.setHasFixedSize(false);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new BuddyAdapter(getContext(),getDataSet(), this::OnClick);
+        mAdapter = new BuddyAdapter(getContext(),getDataSet(), this::OnClick,false);
         mRecyclerView.setAdapter(mAdapter);
 
-        mInput = (EditText) view.findViewById(R.id.input);
+        mInput = getActivity().findViewById(R.id.input);
         Button mSearch = view.findViewById(R.id.search);
         mSearch.setOnClickListener(new View.OnClickListener(){
 
@@ -74,11 +79,45 @@ public class FindBuddiesFragment extends Fragment implements BuddyAdapter.OnClic
     }
 
     private void listenForData() {
-        DatabaseReference usersDb = FirebaseDatabase.getInstance().getReference().child("users");
-        Query query;
-      /*  if(mInput.getText() != null)
-           query = usersDb.orderByChild("userName").startAt(mInput.getText().toString()).endAt(mInput.getText().toString() + "\uf8ff");
-*/
+        results.add(new BuddiesObject("Henry","18","default","eafesfewfewf"));
+        DatabaseReference usersDb = FirebaseDatabase.getInstance().getReference("Users");
+       // Query query = usersDb.orderByChild("userName").startAt(mInput.getText().toString()).endAt(mInput.getText().toString() + "\uf8ff");
+        usersDb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String user_name = "";
+                    String profileUrl = "";
+                    String age = "";
+                   String uid = dataSnapshot.getRef().getKey();
+                   // String uid = dataSnapshot.child("uid").getValue().toString();
+                    if (dataSnapshot.child("userName").getValue() != null) {
+                        user_name = dataSnapshot.child("userName").getValue().toString();
+                    }
+                    if (dataSnapshot.child("age").getValue() != null) {
+                        age = dataSnapshot.child("age").getValue().toString();
+                    }
+                    if (dataSnapshot.child("profilepic").getValue() != null) {
+                        profileUrl = dataSnapshot.child("profilepic").getValue().toString();
+                    }
+                    if (!uid.equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                        BuddiesObject object = new BuddiesObject(user_name, age, profileUrl, uid);
+                        if(!results.contains(object)) {
+                            results.add(object);
+                            mAdapter.notifyDataSetChanged();
+                        }
+                         mRecyclerView.getRecycledViewPool().clear();
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        /*
         usersDb.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
@@ -122,7 +161,7 @@ public class FindBuddiesFragment extends Fragment implements BuddyAdapter.OnClic
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        });
+        });*/
     }
 
     private void clear() {
@@ -132,8 +171,8 @@ public class FindBuddiesFragment extends Fragment implements BuddyAdapter.OnClic
         mAdapter.notifyItemRangeChanged(0,size);
     }
 
-    private ArrayList<BuddiesObject> results = new ArrayList<>();
-    private ArrayList<BuddiesObject> getDataSet() {
+
+    private List<BuddiesObject> getDataSet() {
         listenForData();
         for(int i = 0; i < results.size();i++){
             Log.i("Results", results.get(i).getUser_name());
